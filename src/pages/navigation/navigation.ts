07e884c3d16ec966f35d942cd2fabfbe310d9248
Khalid <khalid@bawes.net>
2018-01-26 13:10:50 +0300

    Functional left side nav

--- src/pages/navigation/navigation.ts
+++ src/pages/navigation/navigation.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, MenuController } from 'ionic-angular';
 
 import { HomePage } from '../home/home';
+import { ContactPage } from '../contact/contact';
 
 @Component({
   selector: 'page-navigation',
@@ -11,11 +12,21 @@ export class NavigationPage {
 
   rootPage = HomePage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  @ViewChild('loggedInContent') mainNav: NavController
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private _menu: MenuController
+  ) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NavigationPage');
+  }
+
+  contact(){
+    this._menu.close();
+    this.mainNav.push(ContactPage);
   }
 
 }
